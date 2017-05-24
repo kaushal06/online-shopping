739c41a35389a12a26b18ae3e540f407cf7028d5
kaushal <kaushal1592@gmail.com>
2017-05-24 06:11:23 -0700

    sucessfully develpoed the static load page

--- onlineshopping/src/main/java/com/kk/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/kk/onlineshopping/controller/PageController.java
@@ -4,15 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 
 public class PageController {
 	@RequestMapping( value="/login",method=RequestMethod.GET)
 	
-	public String index(Model model)
+	public String index(@RequestParam( value="username",required=false) String username,@RequestParam(value="password",required=false) String password ,Model model)
 	{
-		model.addAttribute("message", "login page");
+		 if(username ==null && password==null)
+		 {
+			 model.addAttribute("message","default user log in" );
+		 }
+		 else
+		 {
+			 model.addAttribute("message",username+": "+ password +"log in on the login page" );
+		 }
+		
 		return "login";
 	}
 }
